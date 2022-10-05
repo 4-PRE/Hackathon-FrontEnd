4f6daacfd4eba674e4101f7b18f304e495af2315
J1min <jmhong101506@gmail.com>
2022-10-05 14:13:17 +0900

    DELETE :: main style

--- styles/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import theme from "./theme";
-
-export const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  color: ${({ theme }) => theme.colors.orange};
-  background-color: ${({ theme }) => theme.colors.black};
-`;
