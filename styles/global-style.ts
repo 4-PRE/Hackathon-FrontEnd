ddd8eb0e8ea89aef98f1461faf300e1dbaf4b0b6
J1min <jmhong101506@gmail.com>
2022-10-05 17:11:48 +0900

    ADD :: 한림고딕체로 폰트 변경

--- styles/global-style.ts
+++ styles/global-style.ts
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
     ${normalize}
     *{
         box-sizing: border-box;
+        font-family: 'HallymGothic-Regular';
     }  
     html {    
         font-size: 20px;    
@@ -16,4 +17,10 @@ export const GlobalStyle = createGlobalStyle`
         cursor: pointer; 
         text-decoration: none; 
     }
+    @font-face {
+    font-family: 'HallymGothic-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2204@1.0/HallymGothic-Regular.woff2') format('woff2');
+    font-weight: 400;
+    font-style: normal;
+}
 `;
