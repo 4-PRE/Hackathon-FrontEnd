8de3bd3d6d6dcd12928af608c77912edc89b712c
justinlee05 <justinlee05@naver.com>
2022-10-05 14:40:56 +0900

    FIX :: head style 2

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,12 @@ import styled from "styled-components";
 const Header: NextPage = () => {
   return (
     <Wrapper>
-      <p>asdf</p>
+      <Logo />
+      <TextsWrapper>
+        <p>복지 알아보기</p>
+        <p>일자리 찾기</p>
+        <p>도움말</p>
+      </TextsWrapper>
     </Wrapper>
   );
 };
@@ -14,9 +19,34 @@ export default Header;
 const Wrapper = styled.div`
   border: 1px solid black;
   width: 100%;
-  height: 50px;
+  height: 60px;
   background-color: gray;
   position: fixed;
   top: 0px;
   right: 0px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Logo = styled.div`
+  width: 30px;
+  height: 30px;
+  border: 1px solid black;
+  margin-left: 50px;
+`;
+
+const TextsWrapper = styled.div`
+  width: 400px;
+  height: 100%;
+  position: absolute;
+  left: 60%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  > p {
+    font-size: 18px;
+  }
 `;
