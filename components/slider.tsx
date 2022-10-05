61aebd0738d7b3770f4f4e52f406a0214cb4a54a
J1min <jmhong101506@gmail.com>
2022-10-05 15:15:12 +0900

    ADD :: 퀵메뉴 추가

--- components/slider.tsx
+++ components/slider.tsx
@@ -1,42 +1,66 @@
-import React from "react";
 import type { NextPage } from "next";
-import { Swiper } from "swiper/react";
-import { SwiperSlide } from "swiper/react";
-import Banner from "../public/img/download.jpg";
+import React from "react";
 import Image from "next/image";
+import Banner from "../public/img/download.png";
+import styled from "styled-components";
 
-import { Navigation, Pagination, Scrollbar, Autoplay } from "swiper";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
-import "swiper/css/autoplay";
+const Container = styled.div`
+  position: relative;
+  margin-left: auto;
+  margin-right: auto;
+  top: -30px;
+  left: 0;
+  right: 0;
+  text-align: center;
+  width: 70%;
+  background-color: gold;
+  z-index: 10;
+  border-radius: 12px;
+`;
+
+const MainText = styled.h1`
+  text-align: center;
+  margin: 0;
+  padding: 20px;
+`;
+
+const MenuItem = styled.div`
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background-color: #333;
+`;
+
+const MenuContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const MenuWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
 
 const Slider: NextPage = () => {
   return (
-    <>
-      <Swiper
-        modules={[Navigation, Pagination, Scrollbar, Autoplay]}
-        spaceBetween={20}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        // scrollbar={{ draggable: true }}
-        // onSlideChange={() => console.log("slide change")}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-      >
-        {[1, 2, 3].map((a, i) => {
-          return (
-            <SwiperSlide key={i}>
-              <Image src={Banner} alt="1" className="slide" />
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
-    </>
+    <div>
+      <Image src={Banner} alt="1" />
+        <Container>
+        <MainText>무엇을 도와드릴까요?</MainText>
+        <MenuContainer>
+          {new Array(5).fill(0).map((data, idx) => {
+            return (
+              <MenuWrapper key={idx}>
+                <MenuItem />
+                <h3>일자리 구직</h3>
+              </MenuWrapper>
+            );
+          })}
+        </MenuContainer>
+      </Container>
+    </div>
   );
 };
 
