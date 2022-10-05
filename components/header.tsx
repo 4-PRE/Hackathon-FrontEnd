b1c318beb585d4c19a23d1f7ee6f6c68e915353c
justinlee05 <justinlee05@naver.com>
2022-10-05 14:12:04 +0900

    Auto stash before merge of "main" and "origin/main"

---
+++ components/header.tsx
@@ -0,0 +1,24 @@
+import { NextPage } from "next";
+import styled from "styled-components";
+
+const Header: NextPage = () => {
+  return (
+    <Wrapper>
+      <p>asdf</p>
+    </Wrapper>
+  );
+};
+
+export default Header;
+
+const Wrapper = styled.div`
+  border: 1px solid black;
+  width: 100%;
+  height: 50px;
+  background-color: gray;
+  position: fixed;
+  top: 0px;
+  right: 0px;
+`;
+
+const Logo = 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import type { NextPage } from "next";
+import Header from "../components/header";
 import * as S from "../styles/style";
 import Slider from "../components/slider";
 
 const Home: NextPage = () => {
   return (
+    <>
+      <Header />
     <S.Container>
       <Slider />
     </S.Container>
+    </>
   );
 };
 
