dadddeb1cca5e5dd92494fd118adf017dd966c68
J1min <jmhong101506@gmail.com>
2022-10-06 13:27:26 +0900

    ADD :: 복지 선택추천기능 추가

--- constant/url.ts
+++ constant/url.ts
@@ -2,4 +2,5 @@ const URL = `http://118.40.3.28:8080`;
 
 export const GET_JOBS_URL = `${URL}/jobs`;
 export const POST_JOBS_SEARCH_URL = `${URL}/jobs`;
-export const POST_WELFARE_URL = `${URL}/welfare`
+export const POST_WELFARE_URL = `${URL}/welfare/wishlist`
+export const GET_WELFARE_URL = `${URL}/welfare`
--- pages/welfare/find.tsx
+++ pages/welfare/find.tsx
@@ -1,29 +1,17 @@
 import axios from "axios";
-import React, { useEffect } from "react";
-import { POST_WELFARE_URL } from "../../constant/url";
+import React from "react";
+import { GET_WELFARE_URL, POST_WELFARE_URL } from "../../constant/url";
 
 const Find = () => {
   // const [result, setResult] = React.useState<string[]>([]);
   // const [result2, setResult2] = React.useState<string[]>([]);
+  const [data, setData] = React.useState([]);
+  const [pageNumber, setPageNumber] = React.useState(0);
+  const [pageCount, setPageCount] = React.useState(0);
 
   type form = [string, string, boolean];
 
-  let form: form[] = [
-    ["저소득자", "LOW_INCOME", false],
-    ["장애인", "DISABLED", false],
-    ["국가유공자", "VETERAN", false],
-  ];
-
-  let form2: form[] = [
-    ["교육 지원", "EDUCATION", false],
-    ["의료 지원", "MEDICAL", false],
-    ["문화생활 지원", "CULTURE", false],
-    ["경제적 지원", "FINANCIAL", false],
-    ["기타 정부부처 소관", "OTHER", false],
-  ];
-
-  const instance = axios.create({ baseURL: POST_WELFARE_URL });
-  const submit = () => {
+  const submit = async (randomNumber: number) => {
     let temp: string[] = [];
     let temp2: string[] = [];
     console.log(form, form2);
@@ -39,12 +27,105 @@ const Find = () => {
         temp2.push(form2[i][1]);
       }
     }
-    const result = instance.get("", {
-      params: { page: 0 },
-      data: { targets: temp, wish_benefits: temp2 },
-    });
-    console.log(result);
+
+    let data = {
+      targets: temp,
+      wish_benefits: temp2,
+    };
+    let config = {
+      method: "patch",
+      url: POST_WELFARE_URL + `/${randomNumber}`,
+      data: data,
+      withCredentials: true,
+    };
+    console.log(config.url);
+
+    await axios(config)
+      .then((res) => {
+        console.log(res);
+        let config = {
+          method: "get",
+          url: GET_WELFARE_URL + `/${randomNumber}?page=${pageNumber}`,
+          data: data,
+          withCredentials: true,
+        };
+
+        axios(config)
+          .then((res) => {
+            console.log(res.data);
+            setData(res.data.list);
+            console.log(res.data.page_count)
+            setPageCount(res.data.page_count);
+            console.log(pageNumber);
+          })
+          .catch((err) => {
+            console.log(err);
+            console.log("get err");
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    console.log(temp, temp2);
   };
+
+  const randomNumber = Math.round(Math.random() * 1000);
+
+  React.useEffect(() => {
+    localStorage.setItem("key", randomNumber.toString());
+    console.log(localStorage.getItem("key"));
+  }, []);
+
+  React.useEffect(() => {
+    let config = {
+      method: "get",
+      url: GET_WELFARE_URL + `/${randomNumber}?page=${pageNumber}`,
+      data: data,
+      withCredentials: true,
+    };
+
+    axios(config)
+      .then((res) => {
+        console.log(res);
+        let config = {
+          method: "get",
+          url: GET_WELFARE_URL + `/${randomNumber}?page=${pageNumber}`,
+          data: data,
+          withCredentials: true,
+        };
+
+        axios(config)
+          .then((res) => {
+            console.log(res.data);
+            setData(res.data.list);
+            setPageCount(res.data.page_count);
+            console.log("pageCount", pageCount);
+            console.log(pageNumber);
+          })
+          .catch((err) => {
+            console.log(err);
+            console.log("get err");
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [pageNumber]);
+
+  let form: form[] = [
+    ["저소득자", "LOW_INCOME", false],
+    ["장애인", "DISABLED", false],
+    ["국가유공자", "VETERAN", false],
+  ];
+
+  let form2: form[] = [
+    ["교육 지원", "EDUCATION", false],
+    ["의료 지원", "MEDICAL", false],
+    ["문화생활 지원", "CULTURE", false],
+    ["경제적 지원", "FINANCIAL", false],
+    ["기타 정부부처 소관", "OTHER", false],
+  ];
 
   return (
     <>
@@ -79,7 +160,33 @@ const Find = () => {
           </div>
         );
       })}
-      <button onClick={submit}>계산하기</button>
+      <button
+        onClick={() => {
+          submit(randomNumber);
+        }}
+      >
+        계산하기
+      </button>
+      {data &&
+        data.map((data_, idx) => {
+          return (
+            <div key={idx} style={{ margin: "50px auto" }}>
+              <div>
+                {data_.department} {data_.division}
+              </div>
+              <div>{data_.summary}</div>
+            </div>
+          );
+        })}
+      {new Array(pageCount).fill(0).map((_, idx) => {
+        <button
+          onClick={() => {
+            setPageNumber(idx);
+          }}
+        >
+          {idx + 1}
+        </button>;
+      })}
     </>
   );
 };
